a2f3e66d755ebd0a713594747c542c86bd7f516e
CAS <camdenspitler@icloud.com>
2026-07-11 16:58:30 -0400

    Fix favicon compatibility

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -89,6 +89,7 @@ function seoBlock(path, { noindex = false } = {}) {
 
   return `<!-- SEO:START -->
     <title>${title}</title>
+    <link rel="icon" href="/favicon.ico" sizes="any" />
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
     <meta name="description" content="${description}" />
